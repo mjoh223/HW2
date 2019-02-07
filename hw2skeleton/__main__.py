900dfeb42d627175a837ead62e5e3859cebedfa9
Matt Johnson <mjoh223@lsu.edu>
2019-02-07 11:09:05 -0800

    added test outputs

--- hw2skeleton/__main__.py
+++ hw2skeleton/__main__.py
@@ -1,6 +1,9 @@
 import sys
 from .io import read_active_sites, write_clustering, write_mult_clusterings
-from .cluster import compute_similarity, cluster_by_partitioning, cluster_hierarchically
+from .cluster import compute_similarity, cluster_by_partitioning, cluster_hierarchically, silhouette, pca
+import matplotlib
+matplotlib.use("TkAgg")
+import seaborn as sns
 
 # Some quick stuff to make sure the program is called correctly
 if len(sys.argv) < 4:
@@ -8,19 +11,31 @@ if len(sys.argv) < 4:
     sys.exit(0)
 
 active_sites = read_active_sites(sys.argv[2])
+#silhouette score for multiple clusters
+for i in range(2, 2):
+    kmeans = cluster_by_partitioning(active_sites, i)
+    similarity_matrix, labels  = kmeans
+    msc = silhouette(similarity_matrix, labels)
+    format_output = [i, msc]
+    print("for {} clusters, the mean silhouette score is {} ".format(*format_output))
 
-print(compute_similarity(active_sites[0],active_sites[1]))
+#plot pca
+kmeans = cluster_by_partitioning(active_sites, 2)
+similarity_matrix, labels  = kmeans
+print(pca(similarity_matrix, labels))
+
+
 
 # Choose clustering algorithm
 if sys.argv[1][0:2] == '-P':
     print("Clustering using Partitioning method")
-    clustering = cluster_by_partitioning(active_sites)
+    clustering = cluster_by_partitioning(active_sites, 2)
     #write_clustering(sys.argv[3], clustering)
-    print(clustering)
+    #print(clustering)
 
 if sys.argv[1][0:2] == '-H':
     print("Clustering using hierarchical method")
     clusterings = cluster_hierarchically(active_sites)
     #write_mult_clusterings(sys.argv[3], clusterings)
-    print(clusterings)
+    #print(clusterings)
 
