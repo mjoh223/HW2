b5042018db08910ae876954c929377d3ac644a43
Matt Johnson <mjoh223@lsu.edu>
2019-02-08 16:41:06 -0800

    added comments to functions

--- hw2skeleton/__main__.py
+++ hw2skeleton/__main__.py
@@ -1,6 +1,6 @@
 import sys
 from .io import read_active_sites, write_clustering, write_mult_clusterings
-from .cluster import compute_similarity, cluster_by_partitioning, cluster_hierarchically, silhouette, pca
+from .cluster import compute_similarity, cluster_by_partitioning, cluster_hierarchically, silhouette, pca, cluster_comparision, unpack
 from sklearn.decomposition import PCA
 import numpy as np
 # Some quick stuff to make sure the program is called correctly
@@ -9,41 +9,52 @@ if len(sys.argv) < 4:
     sys.exit(0)
 
 active_sites = read_active_sites(sys.argv[2])
-#silhouette score for multiple clusters
-msc_list = np.zeros(len(range(2,10)))
-for ii, i in enumerate(range(2, 10)):
-    kmeans = cluster_by_partitioning(active_sites, i)
-    similarity_matrix, labels  = kmeans
-    msc = silhouette(similarity_matrix, labels)
-    msc_list[ii] = msc
-np.savetxt("msc.csv", msc_list)
+################silhouette score for multiple clusters######
+#msc_list = np.zeros(len(range(2,10)))
+#for ii, i in enumerate(range(2, 10)):
+#    kmeans = cluster_by_partitioning(active_sites, i)
+#similarity_matrix, labels  = kmeans
+#    msc = silhouette(similarity_matrix, labels)
+#    msc_list[ii] = msc
+#np.savetxt("msc.csv", msc_list)
+#single_link = cluster_hierarchically(active_sites)
+#sim_matrix, labels_single = single_link
 
-single_link = cluster_hierarchically(active_sites)
-sim_matrix, labels_single = single_link
-
-#plot pca
-n_components = 3
-pca = PCA(n_components = n_components)
-X_pca = pca.fit_transform(similarity_matrix) 
-X_pca = np.array(X_pca)
-labels = np.array(labels).astype(int)
-stack = np.column_stack((X_pca, labels, labels_single))
+####################PCA###################################
+#n_components = 3
+#pca = PCA(n_components = n_components)
+#X_pca = pca.fit_transform(similarity_matrix) 
+#X_pca = np.array(X_pca)
+#labels = np.array(labels).astype(int)
+#stack = np.column_stack((X_pca, labels, labels_single))
 #print(stack)
 #np.savetxt("pca_clustering.csv", stack, delimiter=",")
 #sns.scatterplot(x = X_pca[:,0], y = X_pca[:,1])
 
+################cluster comparision#######################
+#clustering_k, labels_k = cluster_by_partitioning(active_sites, 2)
+#clustering_s, labels_s = cluster_hierarchically(active_sites)
+#labels = [labels_k, labels_s]
+#print(cluster_comparision(labels))
 
+clustering_k, labels_k = cluster_by_partitioning(active_sites, 2)
+cluster_list = []
+for x, i in enumerate(labels_k):
+    if i == 1:
+        cluster = active_sites[x]
+        cluster_list.append(cluster)
+print(unpack(cluster_list))
 
 # Choose clustering algorithm
 if sys.argv[1][0:2] == '-P':
     print("Clustering using Partitioning method")
     clustering = cluster_by_partitioning(active_sites, 2)
-    #write_clustering(sys.argv[3], clustering)
+    write_clustering(sys.argv[3], clustering)
     #print(clustering)
 
 if sys.argv[1][0:2] == '-H':
     print("Clustering using hierarchical method")
     clusterings = cluster_hierarchically(active_sites)
-    #write_mult_clusterings(sys.argv[3], clusterings)
+    write_mult_clusterings(sys.argv[3], clusterings)
     #print(clusterings)
 
--- hw2skeleton/cluster.py
+++ hw2skeleton/cluster.py
@@ -6,6 +6,7 @@ from sklearn.cluster import KMeans
 from sklearn.cluster import AgglomerativeClustering
 from sklearn.metrics import silhouette_score
 from sklearn.decomposition import PCA
+
 def compute_similarity(site_a, site_b):
     """
     Compute the similarity between two given ActiveSite instances.
@@ -20,11 +21,17 @@ def compute_similarity(site_a, site_b):
             a = site_a.residues[i].atoms[j].coords
             site_a_coords.append(a)
     site_b_coords = []
+    #unpack the site_b and save the coords
     for i in range(0, len(site_b.residues)):
         for j in range(0, len(site_b.residues[i].atoms)): 
             b = site_b.residues[i].atoms[j].coords
             site_b_coords.append(b)
-
+    #compute the Root-mean-square deviation
+    #low RMSD values (close to 0) are similar structures, compared to higher RMSD values (>>1) are dissimilar
+    #RMSD values are never negative
+    #Generally, the RMSD calculation is the square root of the standard deviation
+    #in atomic structures, the RMSD calculation is the square root of the distance between two atoms with x,y,z coords
+    #the package implemented here performs rotations to find the lowest RMSD
     similarity = rmsd.rmsd(site_a_coords, site_b_coords)
 
     return similarity
@@ -38,10 +45,14 @@ def cluster_by_partitioning(active_sites, num_of_clusters):
             ActiveSite instances)
     """
     n = num_of_clusters
-    rmsd_matrix = np.eye(len(active_sites)) 
+    rmsd_matrix = np.eye(len(active_sites)) #initialize an empty matrix 
     for i in range(0, len(active_sites)):
         for j in range(0,len(active_sites)):
-            rmsd_matrix[i,j] = compute_similarity(active_sites[i], active_sites[j])
+            rmsd_matrix[i,j] = compute_similarity(active_sites[i], active_sites[j]) #perform RMSD between all PDBs and create matrix
+   #K-means imported form sklearn
+   #K means randomly selects K clusters and divides the elements into voronoi cells 
+   #the next step is to update the cluster centers to be the mean of the centroids of the assigned clusters
+   #this is repeated until the labels no longer change. I used the default number of iterations of 10
     kmeans = KMeans(n_clusters=n, random_state=10).fit(rmsd_matrix)
     
     return [rmsd_matrix, kmeans.labels_]
@@ -56,15 +67,19 @@ def cluster_hierarchically(active_sites):
     Output: a list of clusterings
             (each clustering is a list of lists of Sequence objects)
     """
-    single_linkage_matrix = np.eye(len(active_sites)) 
+    single_linkage_matrix = np.eye(len(active_sites)) #initialize an empty matrix
     for i in range(0, len(active_sites)):
         for j in range(0, len(active_sites)):
-            single_linkage_matrix[i,j] = compute_similarity(active_sites[i], active_sites[j])
+            single_linkage_matrix[i,j] = compute_similarity(active_sites[i], active_sites[j]) #perform RMSD between all PDBs and create matrix
+    #Single linkage agglomerative clustering works by assigning every element to its own cluster and finding the nearest neighbor to bottom-up create clusters.
+    #The single linkage method uses the closest neighbor of the clusters as the distance metric compared the the farthest or the average member.
     clustering = AgglomerativeClustering(linkage='single').fit(single_linkage_matrix)
     
     return [single_linkage_matrix, clustering.labels_]
 
 def silhouette(similarity_matrix, predicted_labels):
+    #silhouette coefficient is between -1 and 1, with higher values indicate a member being very similar to the cluster its in. This function
+    #outputs the mean silhouette of the all elements. The silhouette is generally a score of how well a given element is assigned to its cluster and how different it is from the next nearest cluster. 
     MSC = silhouette_score(similarity_matrix, predicted_labels)
     return MSC
 
@@ -74,3 +89,21 @@ def pca(similarity_matrix, predicted_labels):
     X_pca = pca.fit_transform(similarity_matrix)
     #colors = ["navy", "red"]
     return X_pca
+
+def cluster_comparision(labels):
+    lab1 = labels[0] #labels for one clustering method
+    lab2 = labels[1] #labels for another clustering method
+    counter = 0
+    for i in range(0, len(lab1)):
+        if lab1[i] == lab2[i]:
+            counter += 1
+    return counter/len(lab1) #this returns a shared labels percentage.
+
+def unpack(sites): #this just returns the active sites of inputs
+    res_list = []
+    for site in sites:
+        for i in range(0, len(site.residues)):
+            res = site.residues
+            res_list.append(res)
+    return res_list
+
